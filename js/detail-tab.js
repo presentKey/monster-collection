f306b8b4fc10a73495469e2e2c9c2ef0f9c4111e
presentKey <kim.hyeonjae4@gmail.com>
2022-11-03 22:36:19 +0900

    improve: Detect positions of tab-panels

--- js/detail-tab.js
+++ js/detail-tab.js
@@ -33,3 +33,34 @@ detailTabButtonList.forEach((button) => {
   button.addEventListener('click', activeTab)
   button.addEventListener('click', scrollToTabPanel)
 })
+
+function detectDetailTabPanelId() {
+  const detailTabItem = document.querySelectorAll('.detail-tab-item')
+  const detailTabPanelIdList = new Array()
+
+  for (let i = 0; i < detailTabItem.length; i++) {
+    detailTabPanelIdList.push(detailTabItem[i].getAttribute('aria-labelledby'))
+  }
+
+  const detailTabPanelList = detailTabPanelIdList.map((panelId) => {
+    const tabPanel = document.querySelector(`#${panelId}`)
+    return tabPanel
+  })
+
+  return detailTabPanelList
+}
+
+function detectTabPanelPosition() {
+  const panelList = detectDetailTabPanelId()
+
+  const detailTabPanelPositionMap = {}
+
+  panelList.forEach((panel) => {
+    const id = panel.getAttribute('id')
+    const position = window.scrollY + panel.getBoundingClientRect().top
+    detailTabPanelPositionMap[id] = position
+  })
+}
+
+window.addEventListener('load', detectTabPanelPosition)
+window.addEventListener('resize', detectTabPanelPosition)
