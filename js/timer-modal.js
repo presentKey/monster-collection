3d61e03af65000eb718943ccaa459f3d87cbb945
presentKey <kim.hyeonjae4@gmail.com>
2022-11-03 17:17:32 +0900

    new: Implement toggling timer-modal

---
+++ js/timer-modal.js
@@ -0,0 +1,25 @@
+const timerModalButton = document.querySelector('.gnb-timer-button')
+const timerModal = document.querySelector('.timer-modal')
+const timerModalCloseButton = timerModal.querySelector('.close-btn')
+const timerModalOverlay = document.querySelector('.overlay')
+
+function openTimerModal() {
+  timerModal.classList.add('is-open')
+  timerModalOverlay.classList.add('is-active')
+}
+
+timerModalButton.addEventListener('click', openTimerModal)
+
+function closeTimerModalclickOverlay() {
+  timerModal.classList.remove('is-open')
+  timerModalOverlay.classList.remove('is-active')
+}
+
+timerModalOverlay.addEventListener('click', closeTimerModalclickOverlay)
+
+function closeTimerModalclickButton() {
+  timerModal.classList.remove('is-open')
+  timerModalOverlay.classList.remove('is-active')
+}
+
+timerModalCloseButton.addEventListener('click', closeTimerModalclickButton)
