53b2c5b59d2f483bd698fb97cb785b44d86a0246
presentKey <kim.hyeonjae4@gmail.com>
2022-11-03 22:34:06 +0900

    improve: Implement scroll to tab-panel

--- js/detail-tab.js
+++ js/detail-tab.js
@@ -1,6 +1,9 @@
 const detailTab = document.querySelector('.detail-tab')
 const detailTabButtonList = detailTab.querySelectorAll('button')
 
+const TOP_HDEDER_MOBILE = 50 + 40
+const TOP_HEADER_DESKTOP = 60 + 50
+
 let currentActiveTab = detailTab.querySelector('.is-active')
 
 function activeTab() {
@@ -13,6 +16,20 @@ function activeTab() {
   }
 }
 
+function scrollToTabPanel() {
+  const tabPanelId = this.parentNode.getAttribute('aria-labelledby')
+  const tabPanel = document.querySelector(`#${tabPanelId}`)
+
+  const scrollAmount =
+    tabPanel.getBoundingClientRect().top -
+    (window.innerWidth >= 768 ? TOP_HEADER_DESKTOP : TOP_HDEDER_MOBILE)
+
+  window.scrollBy({
+    top: scrollAmount,
+  })
+}
+
 detailTabButtonList.forEach((button) => {
   button.addEventListener('click', activeTab)
+  button.addEventListener('click', scrollToTabPanel)
 })
