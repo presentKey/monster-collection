ad68c5b5bc0573ea87ccc235d92a72f128f13bd4
presentKey <kim.hyeonjae4@gmail.com>
2022-11-10 02:34:24 +0900

    fix: Fix reference error in index.html

--- js/timer-modal.js
+++ js/timer-modal.js
@@ -1,23 +1,24 @@
+const getTimerModal = document.querySelector('.timer-modal')
 const timerModalButton = document.querySelector('.gnb-timer-button')
-const timerModalCloseButton = timerModal.querySelector('.close-btn')
+const timerModalCloseButton = getTimerModal.querySelector('.close-btn')
 const timerModalOverlay = document.querySelector('.overlay')
 
 function openTimerModal() {
-  timerModal.classList.add('is-open')
+  getTimerModal.classList.add('is-open')
   timerModalOverlay.classList.add('is-active')
 }
 
 timerModalButton.addEventListener('click', openTimerModal)
 
 function closeTimerModalclickOverlay() {
-  timerModal.classList.remove('is-open')
+  getTimerModal.classList.remove('is-open')
   timerModalOverlay.classList.remove('is-active')
 }
 
 timerModalOverlay.addEventListener('click', closeTimerModalclickOverlay)
 
 function closeTimerModalclickButton() {
-  timerModal.classList.remove('is-open')
+  getTimerModal.classList.remove('is-open')
   timerModalOverlay.classList.remove('is-active')
 }
 
