2b7cf11b1dee15cc6fed584fb9d8d77290ae38ca
presentKey <kim.hyeonjae4@gmail.com>
2022-11-10 02:29:20 +0900

    style: Implement mobile page-carousel style

--- js/carousel.js
+++ js/carousel.js
@@ -1,6 +1,8 @@
 const pageCarousel = tns({
   container: '.page-carousel .slider-list',
   controls: false,
+  gutter: 6,
+  edgePadding: 20,
   navContainer: '.page-carousel .thumbnail-list',
   navAsThumbnails: true,
   autoplay: true,
