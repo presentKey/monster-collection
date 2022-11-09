00cd63013496972c8817eb8deef3b2d6c5c49a13
presentKey <kim.hyeonjae4@gmail.com>
2022-11-10 04:23:18 +0900

    improve: Improve to process only bookmark data in local storage

--- js/bookmark.js
+++ js/bookmark.js
@@ -2,7 +2,10 @@ const informationMonsterCard = document.querySelectorAll(
   '.information .monster-card'
 )
 const bookmarkPage = document.querySelector('.bookmark .title')
-const deleteAllButton = document.querySelector('.bookmark .delete-all-btn')
+const bookmarkText = 'bookmark'
+
+let bookmarkDeleteButtonList
+let deleteAllButton
 
 function saveBookmark() {
   const monsterCard = this
@@ -15,8 +18,10 @@ function saveBookmark() {
     bookmarkInformationArray.push(registerHTML.outerHTML)
   }
 
+  const localStorageKey = bookmarkText + monsterName
+
   window.localStorage.setItem(
-    monsterName,
+    localStorageKey,
     JSON.stringify(bookmarkInformationArray)
   )
 }
@@ -27,59 +32,80 @@ informationMonsterCard.forEach((image) => {
 
 function showBookmark() {
   const bookmarkDetail = document.querySelector('.bookmark-detail')
+  let contentCount = 0
 
   for (let i = 0; i < window.localStorage.length; i++) {
-    const name = window.localStorage.key(i)
-    const HTML = window.localStorage.getItem(name)
-    const bookmarkWrapper = `<div class="content-wrapper">
-      <button class="delete-btn" type="button">
-        <i class="ic-close"></i>
-      </button>
-    </div>`
+    if (window.localStorage.key(i).startsWith(bookmarkText)) {
+      const name = window.localStorage.key(i)
+      const HTML = window.localStorage.getItem(name)
 
-    bookmarkDetail.insertAdjacentHTML('beforeend', bookmarkWrapper)
+      const bookmarkWrapper = `<div class="content-wrapper">
+        <button class="delete-btn" type="button">
+          <i class="ic-close"></i>
+        </button>
+      </div>`
 
-    const bookmarkContent = document.querySelectorAll('.content-wrapper')
+      bookmarkDetail.insertAdjacentHTML('beforeend', bookmarkWrapper)
 
-    for (const content of JSON.parse(HTML)) {
-      bookmarkContent[i].insertAdjacentHTML('beforeend', content)
+      const bookmarkContent = document.querySelectorAll(
+        '.bookmark .content-wrapper'
+      )
+
+      for (const content of JSON.parse(HTML)) {
+        bookmarkContent[contentCount].insertAdjacentHTML('beforeend', content)
+      }
+      contentCount++
+    } else {
+      continue
     }
   }
+  updatDeleteButton()
 }
 
 if (bookmarkPage !== null) {
-  showBookmark()
+  window.addEventListener('load', showBookmark)
 }
 
-let bookmarkDeleteButtonList = document.querySelectorAll(
-  '.bookmark .delete-btn'
-)
+function updatDeleteButton() {
+  bookmarkDeleteButtonList = document.querySelectorAll('.bookmark .delete-btn')
+  deleteAllButton = document.querySelector('.bookmark .delete-all-btn')
+
+  bookmarkDeleteButtonList.forEach((button) => {
+    button.addEventListener('click', deleteBookmark)
+  })
+  deleteAllButton.addEventListener('click', deleteAllBookmark)
+}
 
 function deleteBookmark() {
   const monsterName =
     this.parentNode.querySelector('.monster-card-name').innerText
   const targetContent = this.parentNode
-
+  const localStorageKey = bookmarkText + monsterName
   targetContent.parentNode.removeChild(targetContent)
-  window.localStorage.removeItem(monsterName)
+  window.localStorage.removeItem(localStorageKey)
 }
-
-bookmarkDeleteButtonList.forEach((button) => {
-  button.addEventListener('click', deleteBookmark)
-})
 
 function deleteAllBookmark() {
   const bookmarkDetail = document.querySelector('.bookmark-detail')
   const contentWrapperAll = document.querySelectorAll(
     '.bookmark .content-wrapper'
   )
+  let deleteNameList = []
+
+  for (let i = 0; i < window.localStorage.length; i++) {
+    if (window.localStorage.key(i).startsWith(bookmarkText)) {
+      const name = window.localStorage.key(i)
+      deleteNameList.push(name)
+    } else {
+      continue
+    }
+  }
 
   for (const item of contentWrapperAll) {
     bookmarkDetail.removeChild(item)
   }
-  window.localStorage.clear()
-}
 
-if (deleteAllButton !== null) {
-  deleteAllButton.addEventListener('click', deleteAllBookmark)
+  for (const keyName of deleteNameList) {
+    window.localStorage.removeItem(keyName)
+  }
 }
