041547f5ce51c40bdf87040b2d54f6f8245e134f
presentKey <kim.hyeonjae4@gmail.com>
2022-11-10 00:39:40 +0900

    new: Import tiny-slider.js and initialize pageCarousel

---
+++ js/carousel.js
@@ -0,0 +1,11 @@
+const pageCarousel = tns({
+  container: '.page-carousel .slider-list',
+  controls: false,
+  navContainer: '.page-carousel .thumbnail-list',
+  navAsThumbnails: true,
+  autoplay: true,
+  autoplayHoverPause: true,
+  autoplayButtonOutput: false,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+})
