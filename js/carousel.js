cae637a3626ce0d3a9b4a8f0b625196017082dc2
presentKey <kim.hyeonjae4@gmail.com>
2022-11-10 21:41:09 +0900

    improve : Implement desktop page-carousel style

--- js/carousel.js
+++ js/carousel.js
@@ -3,6 +3,8 @@ const pageCarousel = tns({
   controls: false,
   gutter: 6,
   edgePadding: 20,
+  controls: true,
+  controlsContainer: '.page-carousel .page-carousel-controls',
   navContainer: '.page-carousel .thumbnail-list',
   navAsThumbnails: true,
   autoplay: true,
@@ -10,4 +12,13 @@ const pageCarousel = tns({
   autoplayButtonOutput: false,
   mouseDrag: true,
   preventScrollOnTouch: true,
+  responsive: {
+    768: {
+      gutter: 8,
+      edgePadding: 36,
+    },
+    1200: {
+      edgePadding: 50,
+    },
+  },
 })
