f14e95ac87308ac0cbd2b829408b7d47b65fa796
presentKey <kim.hyeonjae4@gmail.com>
2022-11-11 16:59:08 +0900

    new: Implement location toast animation

--- js/clipboard.js
+++ js/clipboard.js
@@ -1,9 +1,26 @@
 const copyButtonList = document.querySelectorAll('.location-info .tag-blue')
+const locationToast = document.querySelector('.location-toast')
+
+function locationToastAnimation() {
+  const toastPopUP = [
+    { top: '-30px', offset: 0 },
+    { top: '15px', offset: 0.7 },
+    { top: '-30px', offset: 1 },
+  ]
+  const toastTiming = {
+    duration: 3000,
+    easing: 'ease',
+  }
+
+  locationToast.animate(toastPopUP, toastTiming)
+}
 
 function copyLocation() {
   const locationText = this.parentNode.querySelector('.main-location').innerText
 
   window.navigator.clipboard.writeText(locationText).then(() => {})
+
+  locationToastAnimation()
 }
 
 copyButtonList.forEach((button) => {
