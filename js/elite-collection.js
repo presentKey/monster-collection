fc0459ddfd6d84bca19db9fef5e1b3b085f7ae34
presentKey <kim.hyeonjae4@gmail.com>
2022-11-17 01:41:24 +0900

    new: Load modifier setting from local storage

--- js/elite-collection.js
+++ js/elite-collection.js
@@ -1,19 +1,41 @@
 const modifierButton = document.querySelector('.set-modifier')
+const setModifier = 'setModifier'
 
-function showModifier(e) {
+function showEliteCollection() {
+  const modifierCheckBox = document.querySelector('.set-modifier input')
+
+  modifierCheckBox.checked = JSON.parse(
+    window.localStorage.getItem(setModifier)
+  )
+
+  showModifier()
+}
+
+window.addEventListener('load', showEliteCollection)
+
+function clickModifierButton(e) {
   if (e.target.tagName === 'LABEL') {
     return
   }
 
+  showModifier()
+}
+
+function showModifier() {
   const modifierCheckBox = document.querySelector('.set-modifier input')
   const nameModifier = document.querySelectorAll('.name-modifier')
-  const setModifier = 'setModifier'
 
-  nameModifier.forEach((modifier) => {
-    modifier.classList.toggle('is-active')
-  })
+  if (modifierCheckBox.checked) {
+    nameModifier.forEach((modifier) => {
+      modifier.classList.add('is-active')
+    })
+  } else {
+    nameModifier.forEach((modifier) => {
+      modifier.classList.remove('is-active')
+    })
+  }
 
   window.localStorage.setItem(setModifier, modifierCheckBox.checked)
 }
 
-modifierButton.addEventListener('click', showModifier)
+modifierButton.addEventListener('click', clickModifierButton)
