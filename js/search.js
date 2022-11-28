9e689a7de5f98b8fc9cb917f49dca2a44aa6a5f6
presentKey <kim.hyeonjae4@gmail.com>
2022-11-29 03:46:35 +0900

    Updates

--- js/search.js
+++ js/search.js
@@ -1,7 +1,6 @@
 const searchIconButton = document.querySelector('.gnb-right .ic-search')
 const searchModal = document.querySelector('.search-modal')
-const searchOverlay = document.querySelector('.overlay')
-
+const searchModalCloseBtn = document.querySelector('.search-modal .close-btn')
 const searchModalSubmitButton = document.querySelector(
   '.search-modal .ic-chevron'
 )
@@ -10,59 +9,176 @@ const searchSubmitButton = document.querySelector(
 )
 const FormInput = document.querySelector('.gnb-left .form-input')
 const modalFormInput = document.querySelector('.search-modal .form-input')
+const matchMedia_768 = matchMedia('screen and (min-width: 768px)').matches
 
-const googleLink =
-  'https://www.google.com/search?q=site%3Apresentkey.github.io%2Fmonster-collection%2F+'
+function loadItems() {
+  return fetch('/monster-collection/data/search.json')
+    .then((response) => response.json())
+    .then((json) => json.searchItems)
+}
+
+function displayItems(items) {
+  let searchList
+
+  if (matchMedia_768) {
+    searchList = document.querySelector('.search .search-list')
+  } else {
+    searchList = document.querySelector('.search-modal .search-list')
+  }
+
+  searchList.innerHTML = items.map((item) => createHTMLString(item)).join('')
+}
+
+function createHTMLString(item) {
+  return `
+  <li class="search-item">
+    <a href="${item.url}">
+      <span class="name">${item.name}</span>
+    </a>
+  </li>
+  `
+}
+
+function setInputEvent(items) {
+  let input
+
+  if (matchMedia_768) {
+    input = document.querySelector('.gnb-left .form-input')
+  } else {
+    input = document.querySelector('.search-modal .form-input')
+  }
+
+  input.addEventListener('input', (event) => updateItems(event, items))
+}
+
+function updateItems(event, items) {
+  const value = event.target.value
+  let matchKeyword = []
+
+  items.forEach((item) => {
+    if (item.name.indexOf(value) != -1) {
+      matchKeyword.push(item)
+    }
+  })
+
+  displayItems(matchKeyword)
+}
 
 function openSearchModal() {
   searchModal.classList.add('is-open')
-  searchOverlay.classList.add('is-active')
+
+  loadItems().then((items) => {
+    displayItems(items)
+    setInputEvent(items)
+  })
 }
 
 searchIconButton.addEventListener('click', openSearchModal)
 
 function closeSearchModal() {
   searchModal.classList.remove('is-open')
-  searchOverlay.classList.remove('is-active')
 }
 
-searchOverlay.addEventListener('click', closeSearchModal)
+searchModalCloseBtn.addEventListener('click', closeSearchModal)
 
-function mobileGoogleSearch() {
-  const inputText = document.querySelector('.search-modal .form-input').value
+function openKeywordBox() {
+  const keywordBox = document.querySelector('.search .search-keyword')
+
+  if (!keywordBox.classList.contains('is-active')) {
+    window.addEventListener('click', onClickOutsidCloseKeywordBox)
+  }
+
+  keywordBox.classList.add('is-active')
+
+  loadItems().then((items) => {
+    displayItems(items)
+    setInputEvent(items)
+  })
+}
+
+FormInput.addEventListener('focus', openKeywordBox)
+
+function onClickOutsidCloseKeywordBox(event) {
+  const search = document.querySelector('.search')
+  const keywordBox = document.querySelector('.search .search-keyword')
+
+  if (!search.contains(event.target)) {
+    keywordBox.classList.remove('is-active')
+    window.removeEventListener('click', onClickOutsidCloseKeywordBox)
+    currentIndex = -1
+  }
+}
+
+function LocationHref() {
+  const matchKeyword = matchMedia_768
+    ? document.querySelector('.search .search-item')
+    : document.querySelector('.search-modal .search-item')
+
+  if (matchKeyword !== null) {
+    location.href = matchKeyword.querySelector('a').getAttribute('href')
+  }
+}
+
+let currentIndex = -1
+let previousItem
+
+function arrowKeyMove(event) {
+  event.preventDefault()
+  const matchKeywordList = matchMedia_768
+    ? document.querySelectorAll('.search .search-item')
+    : document.querySelectorAll('.search-modal .search-item')
+  const length = matchKeywordList.length
+
+  if (event.keyCode === 38 || event.keyCode === 40) {
+    if (previousItem !== undefined) {
+      previousItem.classList.remove('is-current')
+    }
+
+    event.keyCode === 38 ? currentIndex-- : currentIndex++
+
+    if (currentIndex >= length) {
+      currentIndex = 0
+    } else if (currentIndex < 0) {
+      currentIndex = length - 1
+    }
+
+    let currentItem = matchKeywordList[currentIndex]
+    currentItem.classList.add('is-current')
+    previousItem = currentItem
+    FormInput.value = currentItem.querySelector('span').innerText
+  }
+}
+
+function onKeydownSearchInput(event) {
+  const inputText = matchMedia_768
+    ? document.querySelector('.gnb-left .form-input').value
+    : document.querySelector('.search-modal .form-input').value
+
+  if (inputText === '') {
+    currentIndex = -1
+  }
+
+  if (inputText !== '' && event.keyCode === 13) {
+    LocationHref()
+  }
+
+  if (event.keyCode === 38 || event.keyCode === 40) {
+    arrowKeyMove(event)
+  }
+}
+
+FormInput.addEventListener('keydown', onKeydownSearchInput)
+modalFormInput.addEventListener('keydown', onKeydownSearchInput)
+
+function onClickSearchButton() {
+  const inputText = matchMedia_768
+    ? document.querySelector('.gnb-left .form-input').value
+    : document.querySelector('.search-modal .form-input').value
 
   if (inputText !== '') {
-    location.href = googleLink + inputText
+    LocationHref()
   }
 }
 
-searchModalSubmitButton.addEventListener('click', mobileGoogleSearch)
-
-function desktopGoogleSearch() {
-  const inputText = document.querySelector('.gnb-left .form-input').value
-
-  if (inputText !== '') {
-    location.href = googleLink + inputText
-  }
-}
-
-searchSubmitButton.addEventListener('click', desktopGoogleSearch)
-
-function keyPressSearch(e) {
-  const inputText = document.querySelector('.gnb-left .form-input').value
-
-  if (inputText !== '' && e.keyCode === 13) {
-    location.href = googleLink + inputText
-  }
-}
-FormInput.addEventListener('keypress', keyPressSearch)
-
-function mobileKeyPressSearch(e) {
-  const inputText = document.querySelector('.search-modal .form-input').value
-
-  if (inputText !== '' && e.keyCode === 13) {
-    location.href = googleLink + inputText
-  }
-}
-
-modalFormInput.addEventListener('keypress', mobileKeyPressSearch)
+searchSubmitButton.addEventListener('click', onClickSearchButton)
+searchModalSubmitButton.addEventListener('click', onClickSearchButton)
